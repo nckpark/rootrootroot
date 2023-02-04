383d3bebc55df99bdd5192b2095c6ae208569a7f
Kevin Horst <krhorst@gmail.com>
2023-02-04 13:24:58 -0800

    Adding raycast support for toggling watched state
    also temp cube growth based on momentum

--- Assets/Scripts/PlayerFocusController.cs
+++ Assets/Scripts/PlayerFocusController.cs
@@ -6,9 +6,55 @@ public class PlayerFocusController : MonoBehaviour
 {
     [SerializeField] private Transform _playerCameraTransform;
 
+    // last watchable watched
+    private Watchable _lastWatchableWatched;
+
     void Update()
     {
         Debug.DrawRay(_playerCameraTransform.position, _playerCameraTransform.forward * 10f, Color.red, 0.1f);
-        Physics.Raycast(_playerCameraTransform.position, _playerCameraTransform.forward, 10);  // TODO layer masks
+        watchOnRaycast();
+        
     }
+
+    void StopWatchingLast()
+    {
+        // if last watchable is set, call stop watching and unset
+        if (_lastWatchableWatched != null)
+        {
+            _lastWatchableWatched.StopWatching();
+            _lastWatchableWatched = null;
+        }
+    }
+
+    void watchOnRaycast()
+    {
+        if (Physics.Raycast(_playerCameraTransform.position, _playerCameraTransform.forward, out RaycastHit hit, 20f))
+        {
+
+            if (hit.collider.gameObject.GetComponent<Watchable>())
+            {
+                watchHitWatchable(hit);
+            }
+            else
+            {
+                StopWatchingLast();
+            }
+        }
+        else
+        {
+            StopWatchingLast();
+        }
+    }
+
+    void watchHitWatchable(RaycastHit hit)
+    {
+        Watchable hitWatchable = hit.collider.gameObject.GetComponent<Watchable>();
+        if (_lastWatchableWatched != hitWatchable)
+        {
+            StopWatchingLast();
+        }
+        _lastWatchableWatched = hitWatchable;
+        _lastWatchableWatched.StartWatching();
+    }
+
 }
